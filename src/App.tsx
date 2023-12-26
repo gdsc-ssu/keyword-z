fb0497fb0cbdad69c9c519a21281a52fda0881e8
choiseohyeon <csh200310@naver.com>
2023-12-26 20:13:53 +0900

    chore/#2/필요없는파일삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/mainHome/home';
+import Home from './pages/mainHome/Home';
 
 function App() {
   return (
