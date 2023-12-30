d0426d1ad9f1e9a5ae28e0cc29bad753f63067d2
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2023-12-30 21:12:44 +0900

    Fix/quiz bug fix (#9)
    * chore/#1/초기세팅
    
    * fix: Quiz.tsx 오류 수정 및 뒤로가기 제거, 정답갯수 , console 주석처리
    
    ---------
    
    Co-authored-by: choiseohyeon <csh200310@naver.com>

--- src/pages/quiz/Quiz.tsx
+++ src/pages/quiz/Quiz.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { IoIosArrowBack } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 
 import styled from 'styled-components';
@@ -19,26 +18,12 @@ const Wrapper = styled.div`
 
 // 맨위 뒤로가기랑 제목 있는 곳
 
-const Top = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-right: 100px;
-  margin-top: 1.5rem;
-`;
-const BackButton = styled(IoIosArrowBack)`
-  color: #ffffff;
-  font-size: 1.5rem;
-  margin-right: 2.5rem;
-  cursor: pointer;
-`;
 const Title = styled.h2`
+  margin-top: 1.5rem;
   color: #fff;
   text-shadow: 4px 4px 3px rgba(255, 255, 255, 0.5);
   font-family: DNF Bit Bit v2;
   font-size: 1.2rem;
-  margin-left: 2rem;
-
 `;
 const SpecialText = styled.span`
   color: var(---, #9cff00);
@@ -63,8 +48,8 @@ const ProgressBar = styled.div`
   overflow: hidden;
 `;
 
-const Progress = styled.div`
-  width: ${(props: ITest) => props.width}%;
+const Progress = styled.div<ITest>`
+  width: ${(props) => props.width}%;
   height: 10px;
   padding: 0;
   text-align: center;
@@ -92,6 +77,7 @@ const Timer = styled.div`
     -2px 2px 0 #000,
     2px 2px 0 #000;
 `;
+
 const TopWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -115,6 +101,7 @@ const Qimg = styled.img`
   width: 350px;
   height: 250px;
 `;
+
 const Qtitle = styled.div`
   display: flex;
   align-items: center;
@@ -128,6 +115,7 @@ const Qtitle = styled.div`
   font-family: DNF Bit Bit v2;
   font-size: 1.2rem;
 `;
+
 const Answers = styled.div`
   padding-top: 3rem;
   display: flex;
@@ -135,29 +123,39 @@ const Answers = styled.div`
   justify-content: center;
   flex-direction: column;
 `;
+
 const Aoption = styled.label`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 280px;
   height: 30px;
-
   border-radius: 10px;
-
   padding: 10px;
   cursor: pointer;
   margin: 10px;
 `;
-const RadioInput = styled.input``;
+// const RadioInput = styled.input`
+//   width: 20px;
+//   height: 20px;
+//   border-radius: 50%;
+//   border: 1px solid #fff;
+//   margin-right: 10px;
+
+//   cursor: pointer;
+//   &:checked {
+//     background-color: #fff;
+//     color: #9cff00;
+//   }
+// `;
+
 const CustomButton = styled.button`
   width: 300px;
   background-color: #ffffff;
   color: black;
-
   text-align: center;
   font-family: DNF Bit Bit v2;
   font-size: 20px;
-
   font-weight: 900;
   line-height: normal;
   padding: 10px;
@@ -174,26 +172,40 @@ const CustomButton = styled.button`
 export default function Quiz() {
   const navigate = useNavigate();
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-
   const [correctAnswersCount, setCorrectAnswersCount] = useState(1);
   const [time, setTime] = useState(20);
-  const [isButtonDisabled, setButtonDisabled] = useState(false);
-  const maxItem = 10;
 
+  const maxItem = 10;
   const availableItem = currentQuestionIndex + 1;
-  const getSeconds = (time) => {
+
+  const getSeconds = () => {
     const seconds = Number(time % 60);
-    if (seconds <= 0 && currentQuestionIndex === 9) {
-      // alert('결과로 넘어갑니다.');
+    if (seconds <= 0) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setTime(20);
     } else if (seconds < 10) {
-      return '0' + String(seconds);
+      return `0${seconds}`;
     } else {
       return String(seconds);
     }
+    return String(seconds);
   };
-  const checkQuizCompletion = () => {
+
+  const handleAnswerClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.currentTarget.value === question[currentQuestionIndex].ans) {
+      setCorrectAnswersCount((prevCount) => prevCount + 1);
+      // console.log(correctAnswersCount);
+    }
+    // else {
+    //   console.log('오답입니다.');
+    // }
     if (currentQuestionIndex === question.length - 1) {
-      alert(`퀴즈가 끝났습니다. 정답 갯수: ${correctAnswersCount}`);
+      navigate('/');
+    } else {
+      setTimeout(() => {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setTime(20);
+      }, 200);
     }
   };
 
@@ -201,66 +213,22 @@ export default function Quiz() {
     const timer = setInterval(() => {
       setTime((prev) => prev - 1);
     }, 1000);
+
     return () => {
       clearInterval(timer);
-
-      if (time <= 0 && currentQuestionIndex <= question.length - 1) {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-        setTime(20);
-        checkQuizCompletion();
-      } else if (time <= 0 && currentQuestionIndex === question.length - 1) {
-        checkQuizCompletion();
-      }
     };
-  }, [currentQuestionIndex, time]);
-
-  const handleAnswerClick = () => {
-    if (!isButtonDisabled) {
-      setButtonDisabled(true);
-      setTimeout(() => {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-        setTime(20);
-        setButtonDisabled(false);
-      }, 300);
-    }
-  };
-  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedAnswer = event.target.value;
-    const correctAnswer = question[currentQuestionIndex].ans;
-    // 정답 갯수를 반환 받아서 나중에 지표로 사용할 듯
-
-    if (selectedAnswer === correctAnswer) {
-      setCorrectAnswersCount((prevCount) => prevCount + 1);
-      console.log(correctAnswer);
-    }
-  };
-  const handleBackClick = () => {
-    if (currentQuestionIndex > 0) {
-      setCurrentQuestionIndex(currentQuestionIndex - 1);
-
-      setTime(20);
-      setButtonDisabled(false);
-    } else {
-      navigate('/');
-    }
-  };
+  }, [currentQuestionIndex, time, correctAnswersCount]);
 
   const currentQuestion = question[currentQuestionIndex];
-  if (!currentQuestion) {
-    alert(`퀴즈가 끝났습니다. 정답 갯수: ${correctAnswersCount}`);
-    return navigate('/');
-  }
+
   const { title, img, options, difficulty } = currentQuestion;
 
   return (
     <MobileFrame>
       <Wrapper>
-        <Top>
-          <BackButton onClick={handleBackClick} />
-          <Title>
-            2023 <SpecialText>MZ</SpecialText> 트렌드 능력고사
-          </Title>
-        </Top>
+        <Title>
+          2023 <SpecialText>MZ</SpecialText> 트렌드 능력고사
+        </Title>
         <ProgressBar>
           <Progress width={(availableItem * 100) / maxItem} />
         </ProgressBar>
@@ -268,20 +236,21 @@ export default function Quiz() {
           <Difficulty>
             Stage {currentQuestionIndex + 1} 난이도 {difficulty}
           </Difficulty>
-          <Timer>{getSeconds(time)}</Timer>
+          <Timer>{getSeconds()}</Timer>
         </TopWrapper>
         <Qtitle>{title}</Qtitle>
         <Qimg src={img} alt="Question Image" />
         <Answers>
-          {options.map((option, index) => (
-            <Aoption key={index}>
-              <RadioInput
+          {options.map((option) => (
+            <Aoption key={option}>
+              {/* <RadioInput
                 type="radio"
                 name="answer"
-                value={option}
-                onChange={(event) => handleRadioChange(event, index)}
-              />
-              <CustomButton disabled={isButtonDisabled} value={option} onClick={handleAnswerClick}>
+                // value={option}
+                // checked={selectedAnswer === option}
+                // onChange={handleRadioChange}
+              /> */}
+              <CustomButton value={option} onClick={handleAnswerClick}>
                 {option}
               </CustomButton>
             </Aoption>
