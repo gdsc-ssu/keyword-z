9c1d3ba2e29d3f484a77ba65602ca536bb52127d
2wndrhs <2wndrhs@gmail.com>
2024-01-01 17:15:41 +0900

    style: css normalize를 위해 GlobalStyle 적용

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,1 +1,21 @@
 // 스타일 모아놓는 파일 (스타일을 따로 파일 빼는건 같이 논의해봐요!)
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  @font-face {
+    font-family: 'SUIT Variable', sans-serif;
+    src: url(https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/variable/woff2/SUIT-Variable.css);
+  }
+  
+  @font-face{
+    font-family:'DNFBitBitv2';
+    font-style:normal;
+    font-weight:400;
+    src:url('//cdn.df.nexon.com/img/common/font/DNFBitBitv2.otf')format('opentype')
+ }
+`;
+
+export default GlobalStyle;
